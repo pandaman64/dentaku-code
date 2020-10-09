db72cae798696b42e35dc8d3820f93bcb4bc84ef
pandaman64 <kointosudesuyo@infoseek.jp>
2020-10-09 21:43:07 +0900

    support CRLF

--- lexer.ts
+++ lexer.ts
@@ -5,7 +5,7 @@ export type Token = {
     width: number,
 }
 
-function match (input: string, res: [Kind, RegExp][]): Token {
+function match (input: string, res: [Kind, RegExp][]): Token | undefined {
   for (const [kind, re] of res) {
     const result = input.match(re)
     if (result !== null) {
@@ -15,21 +15,18 @@ function match (input: string, res: [Kind, RegExp][]): Token {
       }
     }
   }
-
-  throw new Error('no match')
 }
 
 export function lex (input: string): Token[] {
-  const NEWLINE = /^\n/
+  const NEWLINE = /^(\r\n|\n)/
   const INTEGERS = /^[0-9]+/
-  const WHITESPACES = /^\s+/
+  const WHITESPACES = /^[ \t]+/
   const PLUS = /^\+/
   const MINUS = /^-/
   const MULT = /^\*/
   const DIV = /^\//
   const OPEN = /^\(/
   const CLOSE = /^\)/
-  const OTHERS = /^[^0-9+\-*/()\n\s]+/
   const RES: [Kind, RegExp][] = [
     [Kind.NewLine, NEWLINE],
     [Kind.Integer, INTEGERS],
@@ -39,16 +36,37 @@ export function lex (input: string): Token[] {
     [Kind.Mult, MULT],
     [Kind.Div, DIV],
     [Kind.ParenOpen, OPEN],
-    [Kind.ParenClose, CLOSE],
-    [Kind.Error, OTHERS]
+    [Kind.ParenClose, CLOSE]
   ]
 
   const ret: Token[] = []
+  let error: Token | undefined
 
   while (input.length > 0) {
     const token = match(input, RES)
-    ret.push(token)
-    input = input.substring(token.width)
+    if (token !== undefined) {
+      if (error !== undefined) {
+        ret.push(error)
+        error = undefined
+      }
+      ret.push(token)
+      input = input.substring(token.width)
+    } else {
+      const codePointLength = String.fromCodePoint(input.codePointAt(0)!).length
+      if (error !== undefined) {
+        error.width += codePointLength
+      } else {
+        error = {
+          kind: Kind.Error,
+          width: codePointLength
+        }
+      }
+      input = input.substring(codePointLength)
+    }
+  }
+
+  if (error !== undefined) {
+    ret.push(error)
   }
 
   return ret
