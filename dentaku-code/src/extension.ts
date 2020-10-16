1e0e080c548e908761a60509ade6d406d0527867
pandaman64 <kointosudesuyo@infoseek.jp>
2020-10-16 22:26:41 +0900

    show evaluation result

--- dentaku-code/src/extension.ts
+++ dentaku-code/src/extension.ts
@@ -1,6 +1,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { lex } from 'dentaku-core/lexer';
+import { Parser } from 'dentaku-core/parser';
+import { cursorRoot, cursorPrettyPrint } from 'dentaku-core/language';
+import { evalFile } from 'dentaku-core/eval';
+
+// Reference code for Inlay calculation
+// https://github.com/rust-analyzer/rust-analyzer/blob/fb2d332f5f6aa45eb282aebdd01de4bc0ef8a39e/editors/code/src/inlay_hints.ts#L46
+
+// Reference config for Inlay calculation
+// https://github.com/rust-analyzer/rust-analyzer/blob/fb2d332f5f6aa45eb282aebdd01de4bc0ef8a39e/editors/code/package.json#L782
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,15 +19,47 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "dentaku-code" is now active!');
 
-    const change = vscode.workspace.onDidChangeTextDocument(ev => {
-        // const text = ev.document.getText()
-        // const tokens = lex(text)
-        // const parser = new Parser(tokens)
-        // const node = parser.parseFile()
-        // const rootCursor = cursorRoot(node)
-        // console.log(cursorPrettyPrint(rootCursor))
-    })
-    context.subscriptions.push(change)
+    const decoration = vscode.window.createTextEditorDecorationType({
+        after: {
+            color: "#A0A0A0F0",
+            backgroundColor: "#11223300",
+            fontStyle: "normal",
+        }
+    });
+
+    let editor: vscode.TextEditor | undefined = undefined;
+    const changeEditor = vscode.window.onDidChangeTextEditorVisibleRanges(ev => {
+        editor = ev.textEditor;
+    });
+    context.subscriptions.push(changeEditor);
+    const changeDoc = vscode.workspace.onDidChangeTextDocument(ev => {
+        const doc = ev.document;
+        const text = doc.getText();
+        const tokens = lex(text);
+        const parser = new Parser(tokens);
+        const node = parser.parseFile();
+        const rootCursor = cursorRoot(node);
+        const results = evalFile(rootCursor);
+        const ranges: vscode.DecorationOptions[] = results
+            .filter(result => result.value !== undefined)
+            .map(result => {
+                const start = doc.positionAt(result.expr.offset);
+                const end = doc.positionAt(result.expr.offset + result.expr.node.width);
+                const range = doc.validateRange(new vscode.Range(start, end));
+                return {
+                    range,
+                    renderOptions: {
+                        after: {
+                            contentText: ` = ${result.value}`
+                        }
+                    }
+                };
+            });
+        if (editor !== undefined) {
+            editor.setDecorations(decoration, ranges);
+        }
+    });
+    context.subscriptions.push(changeDoc);
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
